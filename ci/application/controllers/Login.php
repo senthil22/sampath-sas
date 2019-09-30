6a772ac6db41eb3904facb48354e14b844deb56b
senthil22 <sencse90@gmail.com>
2019-10-01 00:16:26 +0530

    Update Login.php

--- ci/application/controllers/Login.php
+++ ci/application/controllers/Login.php
@@ -16,35 +16,39 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('login');
-	}
+		#check user logged in or not.
+		if(!empty($this->session->userdata('username')))
+		{
+			redirect('../dashboard');
+		}
 
-	public function login_process()
-	{
-		$this->load->model('user_model');
-		$username=$this->input->post('username');
-		$password=$this->input->post('password');
-		$log=$this->user_model->validate_login($username,$password);
-		
-		if ($log->username){	
-			$sessionData = array(
-				   'username'  	=> $log->username,
-				   'name'  		=> $log->name,
-				   'role'	 	=> $log->role
-				);
-			$ret = array(
-					'logged'	=> TRUE,
-					'to'		=> '/program'
-				);
-			$this->session->set_userdata($sessionData);
-			$this->output->set_header('location:../dashboard');	
-		}else{			
-			$ret['msg']=$log['msg'];
+		if($this->input->post()) {
+			$this->load->model('user_model');
+			$username=$this->input->post('username');
+			$password=$this->input->post('password');
+			$log=$this->user_model->validate_login($username,$password);
+			
+			if (!empty($log->username)) {
+				$sessionData = array(
+					'username' 	=> $log->username,
+					'name'  	=> $log->name,
+					'role'	 	=> $log->role
+					);
+				$ret = array(
+						'logged'	=> TRUE
+					);
+				$this->session->set_userdata($sessionData);
+				$this->output->set_header('location:dashboard');
+			}else{			
+				$ret['msg']=$log['msg'];
+				$this->load->view('login', $ret);
+			}
+		} else {
+			$ret['msg']= '';
 			$this->load->view('login', $ret);
 		}
-		//$this->output->set_content_type('application/json')->set_output(json_encode($ret));
 	}
-
+	
 	public function logout()
 	{		
 		$this->session->sess_destroy();
