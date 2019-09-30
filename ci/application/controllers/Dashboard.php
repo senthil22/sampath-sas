713dc40d7d2e137a95e359374f05e683776e7bc8
senthil22 <sencse90@gmail.com>
2019-10-01 00:15:58 +0530

    Update Dashboard.php

--- ci/application/controllers/Dashboard.php
+++ ci/application/controllers/Dashboard.php
@@ -1,5 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
@@ -12,10 +11,18 @@ class Dashboard extends CI_Controller {
 		$this->load->helper('url');
 		$this->load->helper('form');
 		$this->load->library('session');
+		#check user logged in or not.
+		if(!$this->session->userdata('username'))
+		{
+			redirect('../login');
+		}
 	}
 
 	public function index()
 	{
-		$this->load->view('dashboard');
+		$this->load->model('dashboard_model');
+		$list['product'] = $this->dashboard_model->get_product();
+		$list['buyer'] = $this->dashboard_model->get_buyer();
+		$this->load->view('dashboard', $list);
 	}
 }
